7232e7ff5668191e348642f4420d81017edceff2
Adris3 <advaitris@gmail.com>
2023-09-30 14:38:46 -0400

    added inputs and labels

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -9,7 +9,6 @@ function Login() {
   const { loggedIn, login } = useAccountContext();
   const navigate = useNavigate();
 
-  <form action="/action_page.php"></form>
 
   const attemptLogin = async () => {
     try {
@@ -30,8 +29,12 @@ function Login() {
     <Page>
       <div className="login-page">
         <h1>Login</h1>
+        <label for= "email">Email</label>
+        <input type="text" id="email" name="email"></input><br></br>
+        <label for= "password">Password</label>
+        <input type="password" id="password" name="password"></input><br></br>
         <button onClick={() => attemptLogin()}>
-          Login (as user set in code)
+          Login 
         </button>
         {message && <p>{message}</p>}
       </div>
