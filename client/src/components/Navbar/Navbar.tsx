0954b07ee2dc07b88e90dccf2d9476061be115e8
Sophie de Jong <sophiedejong@cmail.carleton.ca>
2023-09-30 13:37:08 -0400

    changed Navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,17 +2,36 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useAccountContext } from "../../context";
 import "./Navbar.style.scss";
+import giraffeimg from "./girrafe.jpg"
 
 function Navbar() {
   const { loggedIn, logout } = useAccountContext();
   const navigate = useNavigate();
+  const css = `
+  .navbar {
+    position: fixed;
+    padding: 15px 32px;
+  }`
+
+  const button = `
+  .navbar__account {
+    background-color: #F56A23;
+    padding: 15px 60px;
+    text-align: center;
+    display: inline-block;
+    font-size: 16px;
+  }`
 
   return (
     <div className="navbar">
+      <style>{css}</style>
       <div className="navbar__logo">
-        <Link to="/">Store Logo</Link>
+        <Link to="/">
+          <img src={giraffeimg} alt="Girrafe lol" width="80" height="80"/>
+        </Link>
       </div>
       <div className="navbar__account">
+        <style>{button}</style>
         {loggedIn() === false ? (
           <>
             <button onClick={() => navigate("/sign-up")}>Sign Up</button>
